125c88b04f336514b556d3976e49ed31b6693ad1
Damian Barrios <barrios.d122@gmail.com>
2021-04-03 15:05:20 -0300

    Player widget

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Image } from "react-native";
 import styles from "./styles";
-import { Song } from "../../types";
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
+
+import { Audio } from "expo-av";
+import { Sound } from "expo-av/build/Audio";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const song = {
   id: "1",
+  uri:
+    "https://mus.djcristiano.tk/top40/Rauw%20Alejandro%20-%20De%20Corazon%20(Ft.%20J%20Balvin).mp3",
   imageUri:
     "https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg",
-  title: "High on You",
+  title: "De Corazon",
   artist: "Helen",
 };
 
 const PlayerWidget = () => {
+  const [sound, setSound] = useState<Sound | null>(null);
+  const [isPlaying, setIsplaying] = useState<boolean>(true);
+
+  const onPlaybackStatusUpdate = (status) => {
+    setIsplaying(status.isPlaying);
+  };
+
+  const playCurrentSong = async () => {
+    if (sound) {
+      await sound.unloadAsync();
+    }
+
+    const { sound: newSound } = await Audio.Sound.createAsync(
+      { uri: song.uri },
+      { shouldPlay: isPlaying },
+      onPlaybackStatusUpdate
+    );
+    setSound(newSound);
+  };
+
+  useEffect(() => {
+    playCurrentSong();
+  }, []);
+
+  const onPlayPausePress = async () => {
+    if (!sound) {
+      return;
+    }
+    if (isPlaying) {
+      await sound.pauseAsync();
+    } else {
+      await sound.playAsync();
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image source={{ uri: song.imageUri }} style={styles.image} />
@@ -23,7 +63,13 @@ const PlayerWidget = () => {
         </View>
         <View style={styles.iconContainer}>
           <AntDesign name="hearto" size={30} color="white" />
-          <FontAwesome name="play" size={30} color="white" />
+          <TouchableOpacity onPress={onPlayPausePress}>
+            <FontAwesome
+              name={isPlaying ? "pause" : "play"}
+              size={30}
+              color="white"
+            />
+          </TouchableOpacity>
         </View>
       </View>
     </View>
