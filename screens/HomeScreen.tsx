f7990ef9ecd0474e3087c4906a159325758db15d
Damian Barrios <barrios.d122@gmail.com>
2021-04-02 22:16:51 -0300

    HomScreen modified

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,44 +1,18 @@
 import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import AlbumCategory from "../components/AlbumCategory";
-
-const albumCategory = {
-  id: "1",
-  title: "Happy",
-  albums: [
-    {
-      id: "1",
-      imageUri:
-        "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1409&q=80",
-      artistsHeadline: "1",
-    },
-    {
-      id: "2",
-      imageUri:
-        "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1409&q=80",
-      artistsHeadline: "2",
-    },
-    {
-      id: "3",
-      imageUri:
-        "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1409&q=80",
-      artistsHeadline: "3",
-    },
-    {
-      id: "4",
-      imageUri:
-        "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1409&q=80",
-      artistsHeadline: "4",
-    },
-  ],
-};
+import albumCategory from "../data/albumCategories";
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <AlbumCategory
-        title={albumCategory.title}
-        albums={albumCategory.albums}
+      <FlatList
+        data={albumCategory}
+        renderItem={({ item }) => (
+          <AlbumCategory title={item.title} albums={item.albums} />
+        )}
+        keyExtractor={(item) => item.id}
       />
     </View>
   );
