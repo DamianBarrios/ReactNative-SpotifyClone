dfd32347a7c390fda089c0ac696fb1676a82e7c0
Damian Barrios <barrios.d122@gmail.com>
2021-04-02 22:41:16 -0300

    added song list component

--- screens/AlbumScreen.tsx
+++ screens/AlbumScreen.tsx
@@ -1,46 +1,10 @@
 import { useRoute } from "@react-navigation/core";
 import React, { useEffect } from "react";
 import { View, Text } from "react-native";
+import albumDetails from "../data/albumDetails";
 
-const album = {
-  id: "11",
-  name: "Good vibes",
-  by: "Spotify",
-  numberOfLikes: 38,
-  imageUri:
-    "https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg",
-  artistsHeadline: "Taylor Swift, Kygo Objective C, Avicii",
-  songs: [
-    {
-      id: "1",
-      imageUri:
-        "https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg",
-      title: "High on You",
-      artist: "Helen",
-    },
-    {
-      id: "2",
-      imageUri:
-        "https://cdn6.f-cdn.com/contestentries/1485199/27006121/5ca3e39ced7f1_thumb900.jpg",
-      title: "Lorem Ipsum",
-      artist: "Artist 2",
-    },
-    {
-      id: "3",
-      imageUri:
-        "https://images-na.ssl-images-amazon.com/images/I/61F66QURFyL.jpg",
-      title: "Hello from the other side",
-      artist: "Helen",
-    },
-    {
-      id: "4",
-      imageUri:
-        "https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg",
-      title: "High on You",
-      artist: "Helen",
-    },
-  ],
-};
+import SongListItem from "../components/SongListItedm";
+import { FlatList } from "react-native-gesture-handler";
 
 const AlbumScreen = () => {
   const route = useRoute();
@@ -52,6 +16,11 @@ const AlbumScreen = () => {
   return (
     <View>
       <Text style={{ color: "white" }}>Hello From Album screen</Text>
+      <FlatList
+        data={albumDetails.songs}
+        renderItem={({ item }) => <SongListItem song={item} />}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 };
